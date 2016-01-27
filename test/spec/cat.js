ee17bcfedff703132f16529386a6fc9fc67e00c3
muji <freeformsystems@gmail.com>
2016-01-27 18:37:09 +0800

    Assert on cat implementation.
    Concatenates markdown ast representations into a single document.
    
    It accepts an array of file names and parses each document as markdown
    injecting each document's direct children as children of the first
    document encountered.

--- test/spec/cat.js
+++ test/spec/cat.js
@@ -3,15 +3,20 @@ var expect = require('chai').expect
 
 describe('cdb:', function() {
 
-  it('should cat single file to ast', function(done) {
+  it('should cat files to ast', function(done) {
     var concat = mk3.cat();
     concat.on('data', function onData(data) {
       expect(data.type).to.eql('Document');
+      expect(data.firstChild.firstChild.literal)
+        .to.eql('Heading (1)');
+      expect(data.lastChild.firstChild.literal)
+        .to.eql('Heading (2)');
     })
     concat.on('finish', function onFinish() {
       done();
     })
-    concat.end(['test/fixtures/heading.md']);
+    concat.end(
+      ['test/fixtures/heading1.md', 'test/fixtures/heading2.md']);
   });
 
 });
