5b293f0e8f6cf4175678aeed7c16b1b987d91a9c
muji <freeformsystems@gmail.com>
2016-01-27 18:11:15 +0800

    Add initial test spec.

---
+++ test/spec/cat.js
@@ -0,0 +1,18 @@
+var expect = require('chai').expect
+  , mk3 = require('../../lib/mk3')
+
+describe('cdb:', function() {
+
+  it('should cat single file to ast', function(done) {
+    expect(mk3).to.be.a('function');
+    var concat = mk3.cat();
+    concat.on('data', function onData(data) {
+      expect(data.type).to.eql('Document');
+    })
+    concat.on('finish', function onFinish() {
+      done();
+    })
+    concat.end(['test/fixtures/heading.md']);
+  });
+
+});
